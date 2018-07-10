16b25c8479ad0d38201574b9f132f0ab0a864234
NostalgiaBox <rfenster@gmail.com>
2018-07-10 00:22:00 -0400

    Added page changes

--- InteractiveStoryV2/ViewController.swift
+++ InteractiveStoryV2/ViewController.swift
@@ -12,7 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +20,14 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "startAdventure" {
+            guard let pageController = segue.destination as? PageController else {
+                return
+            }
+            pageController.page = Adventure.story
+        }
+    }
 
 }
 
